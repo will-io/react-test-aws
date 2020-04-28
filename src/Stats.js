7481913235271775c671b9f9912be0e2efc53b41
will-io <wesquive@gmu.edu>
2020-04-28 02:29:39 -0400

    beautify

--- src/Stats.js
+++ src/Stats.js
@@ -41,38 +41,40 @@ const Stats = () => {
         <option>Name a-z</option>
       </select>
     </div>
-    <div >
-    <ol>
-      {reviews.map((name)=>
 
-      <li key= {name.id}>
-       
-        <p> Reviewer: {name.name} </p>
-        <p> Location: {name.location} </p>
+
+    <div >
+    <ol className= "ol">
+      {reviews.map((name)=>
+      <li className="li" key= {name.id} >
+          Reviewer: {name.name} 
+          <br  />
+          Location: {name.location} 
         <table border = '1px solid black' align = 'center'>
         <thead>
-          <tr> 
-            <th>Bonus Features:</th> 
+          <tr > 
+            <th align = 'center' >Bonus Features:</th> 
             </tr>   
             </thead>
             <tbody>
                 <tr> 
-          {name.wifi? <td>Wifi &#10004;</td> : <td></td> }
-          {name.outlets? <td>Outlets &#10004;</td> : <td></td> }
-          {name.kid? <td>Kid Friendly &#10004;</td> : <td></td> }
-          {name.reward? <td>Reward Program &#10004;</td> : <td></td> }
-          {name.sofas? <td>Sofa(s) &#10004;</td> : <td></td> }
-          {name.order? <td> Order ahead &#10004;</td> : <td></td> }
-          {name.gluten? <td>Guten-Free Options&#10004;</td> : <td></td> }
-          {name.vegan? <td>Vegan Options &#10004;</td> : <td></td> }
+          {name.wifi? <td>Wifi &#10004;</td> : null }
+          {name.outlets? <td>Outlets &#10004;</td> : null }
+          {name.kid? <td>Kid Friendly &#10004;</td> :null}
+          {name.reward? <td>Reward Program &#10004;</td> : null}
+          {name.sofas? <td>Sofa(s) &#10004;</td> : null }
+          {name.order? <td> Order ahead &#10004;</td> : null }
+          {name.gluten? <td>Guten-Free Options&#10004;</td> : null }
+          {name.vegan? <td>Vegan Options &#10004;</td> : null}
               </tr>
               </tbody>
           </table>
-       <p> Review: {name.review} </p>
-        
+        Review: {name.review} 
       </li>
+    
       )} 
     </ol>
+    <br  />
     </div>
     </div>
 
